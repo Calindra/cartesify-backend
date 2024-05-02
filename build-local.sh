7d7a200b21de6ffb3528bba16b64ea3e0ae6b1a5
Fabio Issamu Oshiro <fabio.oshiro@gmail.com>
2024-05-02 11:58:58 -0300

    build: update @deroll/app to v0.6.0

--- build-local.sh
+++ build-local.sh
@@ -1,7 +1,8 @@
 npm run build
 npm pack
-cp calindra-cartesify-backend-0.1.0.tgz ../cartesify-nodejs-rest-example/backend
+export VERSION=1.0.0
+cp calindra-cartesify-backend-${VERSION}.tgz ../cartesify-nodejs-rest-example/backend
 
 cd ../cartesify-nodejs-rest-example/backend
 npm uninstall @calindra/cartesify-backend
-npm i ./calindra-cartesify-backend-0.1.0.tgz
+npm i ./calindra-cartesify-backend-${VERSION}.tgz
